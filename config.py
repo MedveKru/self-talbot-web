84c519aaa11eb7a242fd32c473c445876df1439d
Dyachenko Artem IU7-13B <artemon1324@yandex.ru>
2022-11-29 18:42:22 +0300

    ADD: config.py <- moved there most of variables for experiment. Marked them static/modifying for user (which vars can be changed in web-version)

---
+++ config.py
@@ -0,0 +1,27 @@
+# --------------------------
+# Static for user values
+
+plt_x_size = 150            # height of a plot
+start_x_axic = plt_x_size   # height of a picture on a plot
+# Should be the same size as plt_x_size
+
+start_z_axic = 0
+end_z_axic = 10_000
+# Weight of a picture on a plot
+
+picture_resolution_x = 7
+picture_resolution_y = 5
+dpi = 125
+# Some kostili for rcParams
+
+plot_resolution_x = int((picture_resolution_x * dpi) / 2) * 2
+plot_resolution_y = int((picture_resolution_y * dpi) / 2) * 2
+# Same for x and z axes
+
+# --------------------------
+# Modifying for user values
+wavelength = 0.6238          # virtually wave's length
+period = 40                  # period of a grid (or a wave??)
+phase_shift = 0
+
+picture_name = 'output.png'  # result picture's name
--- main.py
+++ main.py
@@ -1,34 +1,34 @@
-from diffractio import plt, np, um, mm, degrees, num_max_processors
+from diffractio import plt, np, um, mm
 from diffractio.scalar_masks_X import Scalar_mask_X
-from diffractio.scalar_masks_XZ import Scalar_mask_XZ
 from diffractio.scalar_sources_X import Scalar_source_X
 from diffractio.scalar_fields_XZ import Scalar_field_XZ
 
-from diffractio.utils_multiprocessing import (_pickle_method, _unpickle_method,
-                                              execute_multiprocessing)
-
 from matplotlib import rcParams
 
-rcParams['figure.figsize'] = (7, 5)
-rcParams['figure.dpi'] = 125
+import config
 
-x = np.linspace(-350 * um, 350 * um, 2048)
-z = np.linspace(0 * um, 10 * mm, 512)
-wavelength = 0.6238 * um
-period = 40 * um
-z_talbot = 2 * period ** 2 / wavelength
+# um - standard diffractio units (not necessary)
+# mm = 1000 * um
+
+rcParams['figure.figsize'] = (config.picture_resolution_x, config.picture_resolution_y)
+rcParams['figure.dpi'] = config.dpi
+
+x = np.linspace(config.start_x_axic, -config.start_x_axic, config.plot_resolution_x)
+z = np.linspace(config.start_z_axic, config.end_z_axic, config.plot_resolution_y)
+wavelength = config.wavelength
+z_talbot = 2 * config.period ** 2 / wavelength
 
 u0 = Scalar_source_X(x, wavelength)
 u0.plane_wave(A=1)
 
 t = Scalar_mask_X(x, wavelength)
-t.ronchi_grating(x0=0 * um, period=40 * um, fill_factor=0.5)
+t.ronchi_grating(x0=config.phase_shift, period=config.period, fill_factor=0.5)
 
 talbot_effect = Scalar_field_XZ(x, z, wavelength)
 talbot_effect.incident_field(u0 * t)
 talbot_effect.BPM()
 
 talbot_effect.draw(kind='intensity')
-plt.ylim(-150 * um, 150 * um)
+plt.ylim(-config.plt_x_size, config.plt_x_size)
 
-plt.savefig('aboba.png')
+plt.savefig(config.picture_name)
