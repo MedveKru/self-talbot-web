0311668b03ec0348735e1dcf71fdca40fc2842cf
Dyachenko Artem IU7-13B <artemon1324@yandex.ru>
2022-12-09 10:18:49 +0300

    ADD: get_front() function, which draw front image of talbot_effect. It saves to file with config.picture_front_name name. + pudge.jpg

--- main.py
+++ main.py
@@ -1,14 +1,39 @@
-from diffractio import plt, np, um, mm
+from diffractio import plt, np, um, mm, degrees
+from diffractio.scalar_fields_XY import Scalar_field_XY
 from diffractio.scalar_masks_X import Scalar_mask_X
+from diffractio.scalar_masks_XY import Scalar_mask_XY
 from diffractio.scalar_sources_X import Scalar_source_X
 from diffractio.scalar_fields_XZ import Scalar_field_XZ
+from diffractio.scalar_sources_XY import Scalar_source_XY
 
 from matplotlib import rcParams
+from matplotlib.pyplot import imshow
 
 import config
 
+
 # um - standard diffractio units (not necessary)
 # mm = 1000 * um
+
+def get_front(z_indent):
+    u1 = Scalar_source_XY(x=z, y=x, wavelength=wavelength)
+    u1.plane_wave(A=1)
+
+    t1 = Scalar_mask_XY(x=z, y=x, wavelength=wavelength)
+    # t1.slit(x0=0, size=50 * um, angle=0 * degrees)
+    t1.ronchi_grating(x0=config.phase_shift, period=config.period, fill_factor=0.5)
+
+    u2 = u1 * t1
+    u3 = u2.RS(z=z_indent, new_field=True)
+    u3.draw()
+
+    plt.title('Эффект Тальбота (фронтальное изображение)')
+    plt.xlabel("X (мк)")
+    plt.ylabel("Y (мк)")
+
+    plt.savefig(config.picture_front_name)
+
+
 
 rcParams['figure.figsize'] = (config.picture_resolution_x, config.picture_resolution_y)
 rcParams['figure.dpi'] = config.dpi
@@ -31,10 +56,11 @@ talbot_effect.incident_field(u0 * t)
 talbot_effect.BPM()
 
 talbot_effect.draw(kind='intensity')
+
 plt.title('Эффект Тальбота')
 plt.xlabel("X (мк)")
-plt.ylabel("Y (мк)")
-plt.ylim(-config.plt_x_size, config.plt_x_size)  # in basic world Y axis goes up
+plt.ylabel("Z (мк)")
+plt.ylim(-config.plt_x_size, config.plt_x_size)  # in basic world Z axis goes up
 # but here X axis go up and Z axis go right, so here can be misunderstanding
 
 # Adding to plot a line and a text to show where is Talbot's length
@@ -47,3 +73,5 @@ if config.show_z_talbot:
                  )
 
 plt.savefig(config.picture_name)
+
+get_front(z_talbot)
